dfe24e234deed0d1fccbca85f5948c0babddc2ee
sreehari.v <sreehari.vv007@gmail.com>
2015-07-10 13:22:05 +0530

    commit

---
+++ src/b1.java
@@ -0,0 +1,43 @@
+import java.util.LinkedList;
+
+public class b1<E> {
+
+    private static final double LOAD_FACTOR_LIMIT = 0.7;
+
+    private int size;
+    private LinkedList<E>[] con;
+
+    public b1() {
+        con  = (LinkedList<E>[])(new LinkedList[10]);
+    }
+
+    public boolean add(E obj) {
+        int oldSize = size;
+        int index = Math.abs(obj.hashCode()) % con.length;
+        if(con[index] == null)
+            con[index] = new LinkedList<E>();
+        if(!con[index].contains(obj)) {
+            con[index].add(obj);
+            size++;
+
+        }
+        if(1.0 * size / con.length > LOAD_FACTOR_LIMIT)
+            resize();
+        return oldSize != size;
+    }
+
+    private void resize() {
+        b1<E> temp = new b1<E>();
+        temp.con = (LinkedList<E>[])(new LinkedList[con.length * 2 + 1]);
+        for(int i = 0; i < con.length; i++){
+            if(con[i] != null)
+                for(E e : con[i])
+                    temp.add(e);
+        }
+        con = temp.con;
+    }
+
+    public int size() {
+        return size;
+    }
+}
